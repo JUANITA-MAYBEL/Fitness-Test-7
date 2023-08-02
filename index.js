42c873e5c14102fe74bfbe7765c12f1df9d6900f
Juanita Richard <juanitachi01@gmail.com>
2023-08-02 01:05:01 +0100

    fitness test

---
+++ index.js
@@ -0,0 +1,55 @@
+const title = document.getElementById("image1");
+
+title.addEventListener("click" , (event) => {
+    event.target.style.background = "blue";
+    event.target.style.color = "yellow"
+});
+
+const title2 = document.getElementById("image2");
+
+title2.addEventListener("click" , (event) => {
+    event.target.style.background = "black";
+    event.target.style.color = "green"
+});
+
+const title3 = document.getElementById("image3");
+
+title3.addEventListener("click" , (event) => {
+    event.target.style.background = "gold";
+    event.target.style.color = "coral"
+});
+
+const title4 = document.getElementById("image4");
+
+title4.addEventListener("click" , (event) => {
+    event.target.style.background = "maroon";
+    event.target.style.color = "white"
+});
+
+const title5 = document.getElementById("image5");
+
+title5.addEventListener("click" , (event) => {
+    event.target.style.background = "purple";
+    event.target.style.color = "grey"
+});
+
+const title6 = document.getElementById("image6");
+
+title6.addEventListener("click" , (event) => {
+    event.target.style.background = "red";
+    event.target.style.color = "pink"
+});
+
+const title7 = document.getElementById("image7");
+
+title7.addEventListener("click" , (event) => {
+    event.target.style.background = "brown";
+    event.target.style.color = "olive"
+});
+
+const title8 = document.getElementById("image8");
+
+title8.addEventListener("click" , (event) => {
+    event.target.style.background = "cyan";
+    event.target.style.color = "magenta"
+});
